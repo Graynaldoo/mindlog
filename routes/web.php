e71ebb2a4bfda800dfd8bcbfa96c1b8702fa85b5
Graynaldoo <24091397081@mhs.unesa.ac.id>
2026-06-02 23:09:50 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\Web\JournalController;
 use App\Http\Controllers\Web\StatisticsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('dashboard'));
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware(['auth', 'verified', 'activity.log'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
